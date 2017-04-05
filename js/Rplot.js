6688aa18ef6b2877965d921ebe57d81ca713a98d
DominiquePaul <dominique.paul@startglobal.org>
2017-04-05 20:19:16 +0200

    header eingefuegt

--- js/Rplot.js
+++ js/Rplot.js
@@ -70,6 +70,7 @@ function buttonClicked(){
 	}
 
 	document.getElementById("Rplot").innerHTML = "";
+	document.getElementById("subheading").style.display="none";
 
 	$.fn.scianimator.defaults.theme = 'dark';
 	$('#Rplot').scianimator({
@@ -111,3 +112,32 @@ function buttonClicked(){
 	});
 	$('#Rplot').scianimator('play');
 }
+
+
+$(function() {
+    // Stick the #nav to the top of the window
+    var nav = $('#nav');
+    var navHomeY = nav.offset().top;
+    var isFixed = false;
+    var $w = $(window);
+    $w.scroll(function() {
+        var scrollTop = $w.scrollTop();
+        var shouldBeFixed = scrollTop > navHomeY;
+        if (shouldBeFixed && !isFixed) {
+            nav.css({
+                position: 'fixed',
+                top: 0,
+                left: nav.offset().left,
+                width: nav.width()
+            });
+            isFixed = true;
+        }
+        else if (!shouldBeFixed && isFixed)
+        {
+            nav.css({
+                position: 'static'
+            });
+            isFixed = false;
+        }
+    });
+});
