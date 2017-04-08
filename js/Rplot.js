141bc022348bb458ef0ff3974744957c5ded0a2d
DominiquePaul <dominique.paul@startglobal.org>
2017-04-08 21:27:28 +0200

    new images on learn anim algorithm

--- js/Rplot.js
+++ js/Rplot.js
@@ -5,43 +5,12 @@
 	    var bildergruppe = ["element1", "element2"];
 		for(i = 0; i<= 29; i++)
 		{
-			bildergruppe[i] = "images/eta=0.01-st=true-data=iris" + "/Rplot" + (i+1) +".png";
+			bildergruppe[i] = "images/1-perceptron/eta=0.01-st=true-data=iris" + "/Rplot" + (i+1) +".png";
 		}
 			
 		$.fn.scianimator.defaults.theme = 'dark';
 		$('#Rplot').scianimator({
-		    'images': [
-		    	bildergruppe[0],
-		    	bildergruppe[1],
-		    	bildergruppe[2],
-		    	bildergruppe[3],
-		    	bildergruppe[4],
-		    	bildergruppe[5],
-		    	bildergruppe[6],
-		    	bildergruppe[7],
-		    	bildergruppe[8],
-		    	bildergruppe[9],
-		    	bildergruppe[10],
-		    	bildergruppe[11],
-		    	bildergruppe[12],
-		    	bildergruppe[13],
-		    	bildergruppe[14],
-		    	bildergruppe[15],
-		    	bildergruppe[16],
-		    	bildergruppe[17],
-		    	bildergruppe[18],
-		    	bildergruppe[19],
-		    	bildergruppe[20],
-		    	bildergruppe[21],
-		    	bildergruppe[22],
-		    	bildergruppe[23],
-		    	bildergruppe[24],
-		    	bildergruppe[25],
-		    	bildergruppe[26],
-		    	bildergruppe[27],
-		    	bildergruppe[28],
-		    	bildergruppe[29]
-		    			],
+		    'images': bildergruppe,
 		    'width': 1500,
 		    'delay': 1000,
 		    'loopMode': 'loop'
@@ -61,7 +30,7 @@ function buttonClicked(){
 	//format: data=cancer / data=iris
 	var data = "data=" + document.getElementById("data1").value;
 
-	var zwischen = "Images/" + eta + standardize + data;
+	var zwischen = "Images/1-perceptron/" + eta + standardize + data;
 
 	var bildergruppe = ["element1", "element2"];
 	for(i = 0; i<= 29; i++)
@@ -74,38 +43,7 @@ function buttonClicked(){
 
 	$.fn.scianimator.defaults.theme = 'dark';
 	$('#Rplot').scianimator({
-	    'images': [
-	    	bildergruppe[0],
-	    	bildergruppe[1],
-	    	bildergruppe[2],
-	    	bildergruppe[3],
-	    	bildergruppe[4],
-	    	bildergruppe[5],
-	    	bildergruppe[6],
-	    	bildergruppe[7],
-	    	bildergruppe[8],
-	    	bildergruppe[9],
-	    	bildergruppe[10],
-	    	bildergruppe[11],
-	    	bildergruppe[12],
-	    	bildergruppe[13],
-	    	bildergruppe[14],
-	    	bildergruppe[15],
-	    	bildergruppe[16],
-	    	bildergruppe[17],
-	    	bildergruppe[18],
-	    	bildergruppe[19],
-	    	bildergruppe[20],
-	    	bildergruppe[21],
-	    	bildergruppe[22],
-	    	bildergruppe[23],
-	    	bildergruppe[24],
-	    	bildergruppe[25],
-	    	bildergruppe[26],
-	    	bildergruppe[27],
-	    	bildergruppe[28],
-	    	bildergruppe[29]
-	    			],
+	    'images': bildergruppe,
 	    'width': 1500,
 	    'delay': 1000,
 	    'loopMode': 'loop'
--- js/Rplot2.js
+++ js/Rplot2.js
@@ -5,43 +5,12 @@
 	    var bildergruppe = ["element1", "element2"];
 		for(i = 0; i<= 29; i++)
 		{
-			bildergruppe[i] = "images/eta=0.01-st=true-data=iris" + "/Rplot" + (i+1) +".png";
+			bildergruppe[i] = "images/2-learnMean/eta=0.01-first" + "/Rplot" + (i+1) +".png";
 		}
 			
 		$.fn.scianimator.defaults.theme = 'dark';
 		$('#Rplot').scianimator({
-		    'images': [
-		    	bildergruppe[0],
-		    	bildergruppe[1],
-		    	bildergruppe[2],
-		    	bildergruppe[3],
-		    	bildergruppe[4],
-		    	bildergruppe[5],
-		    	bildergruppe[6],
-		    	bildergruppe[7],
-		    	bildergruppe[8],
-		    	bildergruppe[9],
-		    	bildergruppe[10],
-		    	bildergruppe[11],
-		    	bildergruppe[12],
-		    	bildergruppe[13],
-		    	bildergruppe[14],
-		    	bildergruppe[15],
-		    	bildergruppe[16],
-		    	bildergruppe[17],
-		    	bildergruppe[18],
-		    	bildergruppe[19],
-		    	bildergruppe[20],
-		    	bildergruppe[21],
-		    	bildergruppe[22],
-		    	bildergruppe[23],
-		    	bildergruppe[24],
-		    	bildergruppe[25],
-		    	bildergruppe[26],
-		    	bildergruppe[27],
-		    	bildergruppe[28],
-		    	bildergruppe[29]
-		    			],
+		    'images': bildergruppe,
 		    'width': 1500,
 		    'delay': 1000,
 		    'loopMode': 'loop'
@@ -52,16 +21,13 @@
 
 
 
-function buttonClicked(){
+function button2Clicked(){
 	//eta=0.01-st=true-data=iris
 	//format:  eta=0.01-
 	var eta = "eta=" + document.getElementById("eta1").value + "-";
-	//format: st-false- / st-true-
-	var standardize = "st=" + document.getElementById("standardize1").value + "-";
-	//format: data=cancer / data=iris
-	var data = "data=" + document.getElementById("data1").value;
+	var startingPoint = document.getElementById("startingPoint").value;
 
-	var zwischen = "Images/" + eta + standardize + data;
+	var zwischen = "Images/2-learnMean/" + eta + startingPoint;
 
 	var bildergruppe = ["element1", "element2"];
 	for(i = 0; i<= 29; i++)
@@ -69,43 +35,14 @@ function buttonClicked(){
 		bildergruppe[i] = zwischen + "/Rplot" + (i+1) +".png";
 	}
 
+	//console.warn(zwischen + "/Rplot" + 1 +".png");
+
 	document.getElementById("Rplot").innerHTML = "";
 	document.getElementById("subheading").style.display="none";
 
 	$.fn.scianimator.defaults.theme = 'dark';
 	$('#Rplot').scianimator({
-	    'images': [
-	    	bildergruppe[0],
-	    	bildergruppe[1],
-	    	bildergruppe[2],
-	    	bildergruppe[3],
-	    	bildergruppe[4],
-	    	bildergruppe[5],
-	    	bildergruppe[6],
-	    	bildergruppe[7],
-	    	bildergruppe[8],
-	    	bildergruppe[9],
-	    	bildergruppe[10],
-	    	bildergruppe[11],
-	    	bildergruppe[12],
-	    	bildergruppe[13],
-	    	bildergruppe[14],
-	    	bildergruppe[15],
-	    	bildergruppe[16],
-	    	bildergruppe[17],
-	    	bildergruppe[18],
-	    	bildergruppe[19],
-	    	bildergruppe[20],
-	    	bildergruppe[21],
-	    	bildergruppe[22],
-	    	bildergruppe[23],
-	    	bildergruppe[24],
-	    	bildergruppe[25],
-	    	bildergruppe[26],
-	    	bildergruppe[27],
-	    	bildergruppe[28],
-	    	bildergruppe[29]
-	    			],
+	    'images': bildergruppe,
 	    'width': 1500,
 	    'delay': 1000,
 	    'loopMode': 'loop'
